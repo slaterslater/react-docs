311962e0215eeb46d22dac0908da416b7b3edfd8
slaterslater <slaterslater@gmail.com>
2020-02-24 16:17:06 -0500

    Introducing JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+
+const user = {
+  firstName: "Tony",
+  lastName: "Baloney",
+  url: "https://farm4.static.flickr.com/3237/3069961268_0403db41fa_b.jpg"
+};
+
+function formatName(user){
+  return `${user.firstName} ${user.lastName}`; 
+}
+
+function getGreeting(user){
+  return `Hello ${(user) ? formatName(user) : "Stranger"}`;
+};
 
 function App() {
   return (
-    <h1>Hello, world</h1>
+  <div>
+  <h1>{getGreeting()}</h1>
+  <figure>
+    <img src={user.url} alt={formatName(user)} width="200" />
+    <figcaption>{formatName(user)}</figcaption>
+  </figure>
+  </div>
   );
 }
 
