3f45d338589ce1bbbcdbd79fda7f73a7274d0483
slaterslater <slaterslater@gmail.com>
2020-02-25 16:20:57 -0500

    State and Lifecycle

--- src/App.js
+++ src/App.js
@@ -24,20 +24,44 @@ function Avatar(props){
   );
 }
 
-function tick(){
-  const tock = <p>It's {new Date().toLocaleTimeString()}</p>;
-  ReactDOM.render(tock, document.getElementById('clock'))
-}
+// 5. State and Lifecycle
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+  
+  componentDidMount(){
+    this.timer = setInterval(()=>{
+      this.tick();
+    }, 1000);
+  }
 
-setInterval(tick, 1000);
+  componentWillUnmount(){
+    clearInterval(this.timer);
+  }
+
+  tick(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <p>The time is {this.state.date.toLocaleTimeString()}</p> 
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
   <>
-  {/* <Welcome name={user.firstName} /> */}
-  <Welcome />
+  <Welcome name="" />
+  <Clock />
   <Avatar user={user} />
-  <div id="clock"></div>
   </>
   );
 }
