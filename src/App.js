dd32d5c3ce809eefa654a8a84d986bc862072150
slaterslater <slaterslater@gmail.com>
2020-02-26 10:38:45 -0500

    Handling Events

--- src/App.js
+++ src/App.js
@@ -56,11 +56,59 @@ class Clock extends React.Component {
   }
 }
 
+// 6. Handling Events
+class Toggle extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(){
+    this.setState((state)=>({
+      isToggleOn : !state.isToggleOn
+    }));
+  }
+
+  render(){
+    return (
+      <button onClick={this.handleClick}>
+        {(this.state.isToggleOn)?'ON':'OFF'}
+      </button>
+    );
+  }
+}
+
+class Counter extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {total:0};
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+   handleClick(){
+    this.setState((state, props)=>({
+      total: state.total + props.increment
+    }));
+  }
+
+  render(){
+    return (
+      <>
+        <button onClick={this.handleClick}>+ {this.props.increment}</button>
+        <p>Total: {this.state.total}</p>
+      </>
+    );
+  }
+}
+
 function App() {
   return (
   <>
   <Welcome name="" />
   <Clock />
+  <Toggle />
+  <Counter increment={1} />
   <Avatar user={user} />
   </>
   );
